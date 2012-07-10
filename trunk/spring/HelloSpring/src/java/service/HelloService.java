ad5c75b7197eaf379c89263769ebd0b5f8178e9d
thiagu.vf@gmail.com <thiagu.vf@gmail.com@fc19d786-3e85-8f9f-5133-ce93d90797d7>
2012-07-10 11:06:34 +0000

    Hello spring. Introducing at spring framework.

---
+++ trunk/spring/HelloSpring/src/java/service/HelloService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author thiago
+ */
+public class HelloService {
+    public static String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+}
