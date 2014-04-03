bf0f7a67192c4592b09972866164c95723229c34
thiagu.vf@gmail.com <thiagu.vf@gmail.com@fc19d786-3e85-8f9f-5133-ce93d90797d7>
2014-04-03 01:25:22 +0000

    Initial import.

---
+++ jsf-k19/src/main/java/br/com/k19/controle/NumeroAleatorioBean.java
@@ -0,0 +1,32 @@
+package br.com.k19.controle;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class NumeroAleatorioBean {
+
+	private int maximo;
+	private int numeroAleatorio;
+
+	public String geraNumeroAleatorio() {
+		this.numeroAleatorio = (int) (Math.random() * this.maximo);
+		return "resposta";
+	}
+
+	public int getMaximo() {
+		return maximo;
+	}
+
+	public void setMaximo(int maximo) {
+		this.maximo = maximo;
+	}
+
+	public int getNumeroAleatorio() {
+		return numeroAleatorio;
+	}
+
+	public void setNumeroAleatorio(int numeroAleatorio) {
+		this.numeroAleatorio = numeroAleatorio;
+	}
+	
+}
